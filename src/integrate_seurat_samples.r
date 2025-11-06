9a5114d635ea9fdfa2bba64f748ba8285dc7dede
lawsonvt <mlawsonvt09@gmail.com>
2025-11-06 10:46:39 -0500

    Added to seurat merge

--- src/integrate_seurat_samples.r
+++ src/integrate_seurat_samples.r
@@ -25,7 +25,25 @@ names(seurat_list) <- unlist(lapply(seurat_list, function(x) {x@project.name}))
 seurat_merged <- merge(x=seurat_list[[1]],
                        y=seurat_list[-1],
                        add.cell.ids=names(seurat_list),
-                       merge.data=T)
+                       merge.data=T) # merge normalized data as well
+
+# save the merged seurat
+SaveSeuratRds(seurat_merged, paste0(out_dir, "all_samples.merged_seurat.RDS"))
+
+# integrate!
+seurat_merged <- FindVariableFeatures(seurat_merged)
+seurat_merged <- ScaleData(seurat_merged)
+seurat_merged <- RunPCA(seurat_merged)
+
+# inspect elbow plot
+ElbowPlot(seurat_merged, ndims=30) + 
+  labs(title=sample_name) +
+  scale_x_continuous(breaks=seq(0,30,5)) +
+  scale_y_continuous(breaks=seq(0,30,5), limits=c(0,NA))
+ggsave(paste0(plot_dir, "merge_seurat.pca_elbow_plot.png"), width=6, height=5, bg="white")
+
+
+
 
 
 
