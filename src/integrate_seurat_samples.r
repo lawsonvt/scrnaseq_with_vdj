8bd94b1de7d2c54c8d1a2181a29ea4811c0fe03b
lawsonvt <mlawsonvt09@gmail.com>
2025-11-06 13:37:13 -0500

    Finished workflow

--- src/integrate_seurat_samples.r
+++ src/integrate_seurat_samples.r
@@ -31,7 +31,6 @@ seurat_merged <- merge(x=seurat_list[[1]],
 # save the merged seurat
 SaveSeuratRds(seurat_merged, paste0(out_dir, "all_samples.merged_seurat.RDS"))
 
-# integrate!
 # no normalization required, since we merge the normalized data as well
 seurat_merged <- FindVariableFeatures(seurat_merged)
 seurat_merged <- ScaleData(seurat_merged)
@@ -43,6 +42,41 @@ ElbowPlot(seurat_merged, ndims=30) +
   scale_x_continuous(breaks=seq(0,30,5)) +
   scale_y_continuous(breaks=seq(0,30,5), limits=c(0,NA))
 ggsave(paste0(out_dir, "merge_seurat.pca_elbow_plot.png"), width=6, height=5, bg="white")
+
+
+# after inspecting the elbowplot
+max_pc_dim <- 20
+
+# first do some clustering with the merged data
+seurat_merged <- FindNeighbors(seurat_merged, dims = 1:max_pc_dim, reduction = "pca")
+seurat_merged <- FindClusters(seurat_merged, resolution = 2, cluster.name = "unintegrated_clusters")
+
+# plot out a umap which will showcase the batch effect
+seurat_merged <- RunUMAP(seurat_merged, dims = 1:max_pc_dim, reduction="pca", reduction.name="umap.unintegrated")
+
+Dimplot(seurat_merged, reduction="umap.unintegrated", group.by= "orig.ident")
+ggsave(paste0(out_dir, "merge_seurat.umap.png"), width=8, height=6)
+
+# INTEGRATE
+seurat_merged <- IntegrateLayers(seurat_merged,
+                                 method=HarmonyIntegration,
+                                 orig.reduction = "pca",
+                                 new.reduction = "harmony",
+                                 verbose = F)
+
+# cluster the harmonized data
+seurat_merged <- FindNeighbors(seurat_merged, dims = 1:max_pc_dim, reduction = "harmony")
+seurat_merged <- FindClusters(seurat_merged, resolution = 2, cluster.name = "harmony_clusters")
+
+# create umap
+seurat_merged <- RunUMAP(seurat_merged, dims = 1:max_pc_dim, reduction="harmony", reduction.name="umap.harmony")
+
+# plot it
+DimPlot(seurat_merged, reduction="umap.harmony", group.by= c("orig.ident","harmony_clusters"))
+ggsave(paste0(out_dir, "integrated_seurat.umap.png"), width=14, height=6)
+
+# save the integrated seurat
+SaveSeuratRds(seurat_merged, paste0(out_dir, "all_samples.intergrated_seurat.RDS"))
 
 
 
