ca044a4da4a855e76550ccb9e600caf3f16e77da
lawsonvt <mlawsonvt09@gmail.com>
2025-12-15 13:40:23 -0500

    Cluster naming workflow, with bad cluster removed

---
+++ src/seurat_cluster_naming.R
@@ -0,0 +1,129 @@
+library(Seurat)
+library(ggplot2)
+library(scRepertoire)
+library(snakecase)
+library(cowplot)
+library(dplyr)
+library(ComplexHeatmap)
+library(circlize)
+library(RColorBrewer)
+library(openxlsx)
+
+out_dir <- "results/seurat_cluster_naming/"
+dir.create(out_dir, showWarnings = F)
+
+# cluster markers as supplied by Lukens Lab
+
+lab_cluster_markers <- list("B Cells"=c("Cd19", "Cd79a", "Ms4a1"),
+                            "Monocytes"=c("Ccr2", "Cd44"),
+                            "Neutrophils"=c("Ly6g", "Ngp", "Mmp8"),
+                            "Macrophages"=c("Pf4", "Mrc1", "Ms4a7"),
+                            "T Cells"=c("Trbc2", "Cd3d", "Lck"),
+                            "Microglia"=c("Sall1", "Hexb", "P2ry12"),
+                            "Proliferating Cells"=c("Mki67", "Ccnb1", "Tpx2"),
+                            "Pericytes"=c("Pdgfrb", "Rgs5"))
+
+# read in integrated seurat
+int_seu <- LoadSeuratRds("results/join_and_filter_integrated_seurat/filtered.integrated_seurat.RDS")
+
+analysis_genes <- rownames(int_seu)
+
+# find any issues between marker genes and analysis genes
+
+lab_cluster_markers_missing <- lapply(lab_cluster_markers, function(gene_list) {
+  
+  gene_list[!gene_list %in% analysis_genes]
+  
+})
+
+# cow dot plot for the markers
+dotplot_list <- lapply(names(lab_cluster_markers), function(cluster_name) {
+  
+  markers <- lab_cluster_markers[[cluster_name]]
+  
+  DotPlot(int_seu, features = markers) + RotatedAxis() + labs(x=NULL, y=NULL, title=cluster_name) +
+    theme(legend.position="none")
+  
+})
+
+plot_grid(plotlist = dotplot_list, nrow = 1)
+ggsave(paste0(out_dir, "total_dotplots.png"), width=18, height=7, bg="white")
+
+# make a cross reference after looking over the dot plot
+
+harmony_clusters <- sort(unique(int_seu@meta.data$harmony_clusters))
+
+cluster_xref <- data.frame(harmony_clusters=harmony_clusters,
+                           cell_cluster=c("Microglia", 
+                                          "Microglia",
+                                          "Microglia",
+                                          "Microglia",
+                                          "Microglia",
+                                          "Microglia",
+                                          "Microglia",
+                                          "Microglia",
+                                          "Macrophages",
+                                          "Microglia",
+                                          "Microglia",
+                                          "Macrophages",
+                                          "Microglia",
+                                          "Microglia",
+                                          "T Cells",
+                                          "Microglia",
+                                          "Neutrophils",
+                                          "B Cells",
+                                          "Monocytes",
+                                          "Proliferating Cells",
+                                          "Monocytes",
+                                          "Pericytes",
+                                          "Macrophages",
+                                          "Monocytes",
+                                          "Proliferating Cells",
+                                          "B Cells",
+                                          "Proliferating Cells"))
+
+# merge them into the metadata
+metadata <- int_seu@meta.data
+# merge in new cluster names
+
+metadata$cell_id <- rownames(metadata)
+
+metadata <- merge(metadata,
+                  cluster_xref,
+                  by="harmony_clusters")
+
+# fix the order
+rownames(metadata) <- metadata$cell_id
+metadata <- metadata[rownames(int_seu@meta.data),]
+
+# add back into object
+int_seu <- AddMetaData(int_seu, metadata$cell_cluster, col.name="cell_cluster")
+
+# plot this out!
+DimPlot(int_seu, reduction="umap.harmony", group.by= c("harmony_clusters", "cell_cluster"),
+        label=T)
+ggsave(paste0(out_dir, "cluster_cells_umap.with_legend.png"), width=14, height=6)
+
+
+p1 <- DimPlot(int_seu, reduction="umap.harmony", group.by= "harmony_clusters",
+              label=T) + labs(x="UMAP 1", y="UMAP 2", title="Harmony Clusters") + 
+  theme(legend.position = "none")
+
+p2 <- DimPlot(int_seu, reduction="umap.harmony", group.by= "cell_cluster",
+              label=T) + labs(x="UMAP 1", y="UMAP 2", title="Cell Types") + 
+  theme(legend.position = "none")
+
+plot_grid(p1, p2, nrow=1)
+ggsave(paste0(out_dir, "cluster_cells_umap.without_legend.png"), width=12, height=6)
+
+
+DimPlot(int_seu, reduction="umap.harmony", group.by="cell_cluster",
+        label=T, split.by = "orig.ident", ncol=4, raster=F) + 
+  theme(legend.position = "none") + labs(x="UMAP 1", y= "UMAP 2", title="Cell Types")
+ggsave(paste0(out_dir, "cluster_cells_umap.per_sample.png"), width=17, height=10)
+
+# save things
+write.xlsx(cluster_xref, paste0(out_dir, "cluster_xref.xlsx"), colWidths="auto")
+
+SaveSeuratRds(int_seu, file=paste0(out_dir, "cell_named.seurat.RDS"))
+
